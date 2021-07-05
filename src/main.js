293f1956bad3c5aa50d52f9fd618cb9b4ed79661
masa-dev <mssa.massan@gmail.com>
2021-07-05 17:49:07 +0900

    不要な変数の削除

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
 
-import { BootstrapVue, IconsPlugin, FormInputPlugin } from "bootstrap-vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-Vue.use(FormInputPlugin);
 
 Vue.config.productionTip = false;
 
