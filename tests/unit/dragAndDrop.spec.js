dc8ccaf920926ca567d6a07b9d1fae533c9dcd86
masa-dev <mssa.massan@gmail.com>
2021-07-07 03:36:20 +0900

    テストコードの修正と改良

--- tests/unit/dragAndDrop.spec.js
+++ tests/unit/dragAndDrop.spec.js
@@ -3,8 +3,13 @@ import DragAndDrop from "@/components/DragAndDrop.vue";
 
 describe("DragAndDrop.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "ここに画像をドロップ または画像を選択";
     const wrapper = shallowMount(DragAndDrop);
-    expect(wrapper.text()).toMatch(msg);
+    const fileInput = wrapper.find("#file-input");
+
+    expect(wrapper.text()).toContain("ここに画像をドロップ");
+    expect(wrapper.text()).toContain("または画像を選択");
+
+    expect(fileInput.exists()).toBe(true);
+    expect(fileInput.element.tagName).toBe("INPUT");
   });
 });
