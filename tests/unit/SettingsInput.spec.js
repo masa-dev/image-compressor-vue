5bf2e4aad1df7111e791deb7794db74a07db3360
masa-dev <mssa.massan@gmail.com>
2021-07-07 03:22:27 +0900

    テストコードを見やすくする等の目的で改良

--- tests/unit/SettingsInput.spec.js
+++ tests/unit/SettingsInput.spec.js
@@ -7,10 +7,19 @@ localVue.use(BootstrapVue);
 
 describe("SettingsInput.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg =
-      "品質： 0.8  最大横幅  最大縦幅  JPEG形式のみ処理を行う すべてJPEG形式に変換（注意）";
     const wrapper = mount(SettingsInput, { localVue });
+    const inputArray = wrapper.findAll("input");
 
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toMatch(/品質：\s(\d|\d\.\d)/);
+    expect(wrapper.text()).toContain("最大横幅");
+    expect(wrapper.text()).toContain("JPEG形式のみ処理を行う");
+    expect(wrapper.text()).toContain("すべてJPEG形式に変換（注意）");
+
+    expect(inputArray.length).toBe(5);
+
+    inputArray.wrappers.forEach((InputWrapper) => {
+      expect(InputWrapper.exists()).toBe(true);
+      expect(InputWrapper.element.tagName).toBe("INPUT");
+    });
   });
 });
