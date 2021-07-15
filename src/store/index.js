e2507df9fe5ec89d3da3529e155d79db5d771146
masa-dev <mssa.massan@gmail.com>
2021-07-16 00:21:43 +0900

    refactor: Vuex store を管理しやすくするために、モジュールに分割する。
    Vuex store が大きくなり管理が難しくなってきたため、モジュールに分割してそれぞれの状態を独立させ管理しやすくする。
    既存のコードに影響を及ぼさないために、出来るだけ名前は変えずにモジュール化した。
    一部変数については、より分かりやすくするために変数名を変更する。
      (ex. imageFiles ==> inputFile.originImages)

--- src/store/index.js
+++ src/store/index.js
@@ -3,49 +3,72 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
-  state: {
-    imageFiles: [],
-    processing: false,
-    settings: {
-      quality: 0.8,
-      maxWidth: Infinity,
-      maxHeight: Infinity,
-      isOnlyJpeg: true,
-      convertAlltoJpeg: false,
-      processWhenFileInput: true,
-      hideFileList: false,
-      callHistoryOnStartup: true,
-    },
-    test: {
-      imageSrc: "",
-      page: 0,
-    },
-  },
+const inputFile = {
+  state: () => ({
+    originImages: [],
+    compressedImages: [],
+  }),
   getters: {
     getFilesLength: (state) => {
-      return state.imageFiles.length;
+      return state.originImages.length;
     },
   },
   mutations: {
-    //同期処理
     setFiles(state, files = []) {
-      state.imageFiles.splice(0, state.imageFiles.length);
+      state.originImages.splice(0, state.originImages.length);
       console.log(files);
-      state.imageFiles.push(...files.images);
+      state.originImages.push(...files.images);
     },
+  },
+  actions: {},
+};
+
+const settings = {
+  state: () => ({
+    quality: 0.8,
+    maxWidth: Infinity,
+    maxHeight: Infinity,
+    isOnlyJpeg: true,
+    convertAlltoJpeg: false,
+    processWhenFileInput: true,
+    hideFileList: false,
+    callHistoryOnStartup: true,
+  }),
+  getters: {},
+  mutations: {
     setSettings(state, setting) {
       const varName = setting.varName;
       const value = setting.value;
 
-      if (typeof state.settings[varName] === "undefined")
+      if (typeof state[varName] === "undefined")
         throw new Error("変数varNameの値を持つプロパティが存在しません。");
 
-      state.settings[varName] = value;
+      state[varName] = value;
     },
   },
-  actions: {}, //非同期処理
-  modules: {},
+  actions: {},
+};
+
+const sideContent = {
+  state: () => ({
+    imageSrc: "",
+    page: 0,
+  }),
+  getters: {},
+  mutations: {},
+  actions: {},
+};
+
+const store = new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    inputFile,
+    settings,
+    sideContent,
+  },
 });
 
 // コンポーネントからは、this.store.~でアクセス可能
