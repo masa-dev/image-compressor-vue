a4a521a983d2c37b8dc8cb664fda13cd77e2339b
masa-dev <mssa.massan@gmail.com>
2021-07-19 21:56:29 +0900

    feat: 画像圧縮とダウンロードのボタンに対応したメソッドの追加
    画像圧縮はutil内にあるcompressImage.jsを実行し、
    ダウンロードは、JSZipとfile-saverの機能を利用して作成。

--- src/components/ExecutionButtons.vue
+++ src/components/ExecutionButtons.vue
@@ -1,9 +1,51 @@
 <template>
   <div id="execution-buttons">
-    <b-button block variant="info">再圧縮処理</b-button>
-    <b-button block variant="primary">画像ファイルをダウンロード</b-button>
+    <b-button block variant="info" @click="recompressImages"
+      >再圧縮処理</b-button
+    >
+    <b-button block variant="primary" @click="downloadImages"
+      >画像ファイルをダウンロード</b-button
+    >
   </div>
 </template>
+
+<script>
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+import compressImages from "@/util/compressImages.js";
+
+export default {
+  methods: {
+    recompressImages() {
+      console.log(this.$store.state.settings);
+      console.log(this.$store.state.inputFile);
+      compressImages(this.$store);
+    },
+    downloadImages() {
+      let files = this.$store.state.files.compressedImages;
+
+      if (files.length === 0) {
+        alert("ファイルを選択してください");
+        return;
+      }
+
+      // TODO: 保存開始時の処理(show Spinner)
+
+      let zip = new JSZip();
+      for (let file of files) {
+        zip.file(file.name, file, { base64: true });
+      }
+
+      zip.generateAsync({ type: "blob" }).then((content) => {
+        // FileSaver.js
+        saveAs(content, "compressed.zip");
+
+        // TODO: 保存終了時の処理(hide Spinner)
+      });
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 #execution-buttons {
